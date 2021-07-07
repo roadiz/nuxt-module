8af402c7fe5db8af140d4a999d2970cf7223f328
Ambroise Maupate <ambroise@rezo-zero.com>
2021-07-07 16:25:05 +0200

    First commit

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
 # nuxt-plugin
-Nuxt API client
+Nuxt Typescript plugin for Roadiz API client.
+
+## Usage
+
+`yarn add @roadiz/nuxt-plugin`
+
+### Configuration
+
+```js
+// nuxt.config.js
+
+plugins: [
+    '@roadiz/nuxt-plugin'
+],
+    
+publicRuntimeConfig: {
+    roadiz: {
+        baseUrl: 'https://myroadizapi.test/api/1.0',
+        apiKey: 'xxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxx',
+        preview: false,
+        debug: false,
+        origin: 'https://mywebsite.test'
+    }
+}
+```
+
+### API SDK
+
+```ts
+async asyncData({ $roadiz, route, req }: Context): Promise<object | void> | object | void {
+    const path = req ? req.url : route.fullPath
+    const response = await $roadiz.getSingleNodesSourcesByPath(path)
+    const pageData = response.data
+    const altLinks = $roadiz.getAlternateLinks(response)
+
+    switch (data["@type"]) {
+        case 'BlogPostListing':
+            const [blogPostsResponse, blogPostTagsResponse] = await Promise.all([
+                $roadiz.getNodesSourcesForType<NSBlogPost>('blogpost'),
+                $roadiz.getTagsForType('blogpost')
+            ])
+
+            return {
+                pageData,
+                altLinks,
+                blogPosts: blocks.data["hydra:member"],
+                blogPostsCount: blocks.data["hydra:totalItems"],
+                tags: tags.data["hydra:member"]
+            }
+    }
+    
+    return {
+        pageData,
+        altLinks
+    }
+}
+```
---
+++ src/plugin.ts
@@ -0,0 +1,72 @@
+import { Context } from '@nuxt/types'
+import { Inject } from '@nuxt/types/app'
+import RoadizApi from '@roadiz/abstract-api-client'
+import { AxiosRequestConfig } from 'axios'
+
+export interface RoadizPluginConfig {
+    baseUrl: string
+    apiKey: string
+    preview?: boolean
+    debug?: boolean
+    origin: string
+}
+
+declare module '@nuxt/types' {
+    interface Context {
+        $roadiz: NuxtRoadizApi
+    }
+}
+
+class NuxtRoadizApi extends RoadizApi {
+    private _context: Context
+    private _origin?: string | null
+
+    constructor(
+        context: Context,
+        baseURL: string,
+        apiKey: string,
+        preview: boolean,
+        debug: boolean,
+        origin: string | null
+    ) {
+        super(baseURL, apiKey, preview, debug)
+        this._context = context
+        this._origin = origin
+    }
+
+    get context(): Context {
+        return this._context
+    }
+
+    get origin(): string | null {
+        return this._origin || null
+    }
+
+    protected onApiRequest(config: AxiosRequestConfig): AxiosRequestConfig {
+        config = super.onApiRequest(config)
+
+        if (process.server && this.context.req && this.context.req.headers) {
+            config.headers.common.origin = this.context.req.headers.origin || this.origin
+            config.headers.common['accept-language'] = this.context.req.headers['accept-language'] || null
+
+            for (const propName of config.headers.common) {
+                if (config.headers.common[propName] === null || config.headers.common[propName] === undefined) {
+                    delete config.headers.common[propName]
+                }
+            }
+        }
+
+        if (process.server) {
+            // Don't accept brotli encoding because Node can't parse it
+            config.headers.common['accept-encoding'] = 'gzip, deflate'
+        }
+
+        return config
+    }
+}
+
+export default function (context: Context, inject: Inject) {
+    const { baseUrl, apiKey, preview, debug, origin } = context.$config.roadiz as RoadizPluginConfig
+
+    inject('roadiz', new NuxtRoadizApi(context, baseUrl, apiKey, preview || false, debug || false, origin || null))
+}
