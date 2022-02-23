8e2c8df8beff7aa73482925e32f18e0b2789bad0
Ambroise Maupate <ambroise@rezo-zero.com>
2022-02-23 15:48:15 +0100

    feat: added preview at request-time (#3)
    * feat (preview): WIP, Added preview at request-time
    
    * fix: manuelodelain review fixes

--- src/plugin.ts
+++ src/plugin.ts
@@ -3,10 +3,18 @@ import { Inject } from '@nuxt/types/app'
 import RoadizApi from '@roadiz/abstract-api-client'
 import { AxiosRequestConfig } from 'axios'
 
+export interface RoadizPreviewJwt {
+    iat: number // issued at (Unix time)
+    exp: number // expiration time (Unix time)
+    roles: Array<string>
+    username: string
+}
+
 export interface RoadizPluginConfig {
     baseURL: string
     apiKey?: string
-    preview?: boolean
+    preview?: boolean // deprecated: Preview mode MUST be set at request time
+    allowClientPreview?: boolean
     debug?: boolean
     origin?: string
     defaults?: AxiosRequestConfig
@@ -14,15 +22,21 @@ export interface RoadizPluginConfig {
 
 export class NuxtRoadizApi extends RoadizApi {
     private _context: Context
+    private _previewing: boolean
+    private _previewingJwt: RoadizPreviewJwt | null
+    private _allowClientPreview: boolean
     private _origin?: string
 
     constructor(context: Context, config: RoadizPluginConfig) {
-        const { baseURL, apiKey, preview, debug, origin, defaults } = config
+        const { baseURL, apiKey, preview, allowClientPreview, debug, origin, defaults } = config
 
         super(baseURL, { apiKey, preview, debug, defaults })
 
         this._context = context
         this._origin = origin
+        this._previewing = false
+        this._previewingJwt = null
+        this._allowClientPreview = allowClientPreview || false
     }
 
     get context(): Context {
@@ -31,6 +45,14 @@ export class NuxtRoadizApi extends RoadizApi {
 
     get origin(): string | undefined {
         return this._origin
+    }
+
+    get isPreviewing(): boolean {
+        return this._previewing
+    }
+
+    get previewingJwt(): RoadizPreviewJwt | null {
+        return this._previewingJwt
     }
 
     protected onApiRequest(config: AxiosRequestConfig): AxiosRequestConfig {
@@ -47,12 +69,48 @@ export class NuxtRoadizApi extends RoadizApi {
             }
         }
 
+        /*
+         * Pass through preview state and JWT token to Roadiz API
+         */
+        if (this._allowClientPreview && this.context.req && this.context.req.url) {
+            const currentUrl = new URL(this.context.req.url)
+            if (
+                currentUrl.searchParams.has('_preview') &&
+                currentUrl.searchParams.get('_preview') === '1' &&
+                currentUrl.searchParams.has('token') &&
+                currentUrl.searchParams.get('token') !== ''
+            ) {
+                config.headers.params['_preview'] = '1'
+                config.headers.common.Authorization = `Bearer ${currentUrl.searchParams.get('token')}`
+                this._previewing = true
+                this._previewingJwt = this.getJwtPayload(currentUrl.searchParams.get('token'))
+            }
+        }
+
         if (process.server) {
             // Don't accept brotli encoding because Node can't parse it
             config.headers.common['accept-encoding'] = 'gzip, deflate'
         }
 
         return config
+    }
+
+    protected getJwtPayload(token: string | null): RoadizPreviewJwt {
+        if (token) {
+            const base64Url = token.split('.')[1]
+            const base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/')
+            const jsonPayload = decodeURIComponent(
+                atob(base64)
+                    .split('')
+                    .map((c) => {
+                        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
+                    })
+                    .join('')
+            )
+
+            return JSON.parse(jsonPayload)
+        }
+        throw new Error('JWT token is null')
     }
 }
 
